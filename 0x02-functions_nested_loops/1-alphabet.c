385920ee406e0294ec0a4adb3fd8ca251a35d71e
Mahlori412 <manganyimahlori6@gmail.com>
2022-06-07 08:29:57 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int al = 'a';
+	while (al <= 'z')
+	{
+		_putchar(al);
+		c++;
+	}
+	_putchar('\n');
+}
