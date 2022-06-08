8f9dde3c2db2547c19d5a63cbbb30f4415e8a7e3
Mahlori412 <manganyimahlori6@gmail.com>
2022-06-07 17:17:20 -0700

    im tired

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
